95b3a306d6c5da5582e3ffb6fc16f83e1a32b87a
4rio19 <nburressivc@gmail.com>
2018-05-03 10:12:49 -0500

    stores API endpoint

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -93,5 +93,17 @@ exports.getStoresByTag = async (req, res) => {
 };
 
 exports.searchStores = async (req, res) => {
-  res.json(req.query);
+  const stores = await Store.find({
+    $text: {
+      $search: req.query.q
+
+    }
+  }, {
+    score: { $meta: 'textScore' }
+  })
+  .sort({
+    score: { $meta: 'textScore' }
+  })
+  .limit(5);
+  res.json(stores);
 };
